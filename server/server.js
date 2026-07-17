5caed48368e2cd52098f09f51e0e64fd41450c8f
Shaheen <shn.sdq29@gmail.com>
2026-07-18 01:05:16 +0530

    fix: update route handling for catch-all in server.js

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.use("/api/users", userRouter);
 
 app.use(express.static(path.join(__dirname, "/client/dist")));
 
-app.get("*", (req,res)=>{
+app.get("/*splat", (req,res)=>{
     res.sendFile(path.join(__dirname, "/client", "dist", "index.html"));
 });
 
